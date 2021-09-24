9ffe4a368b6a33e819615a9ce0cd8d4db32e2368
JT <67124242+JTVong@users.noreply.github.com>
2021-09-24 12:08:59 -0700

    Update servers.test.js

--- servers.test.js
+++ servers.test.js
@@ -1,22 +1,22 @@
-// describe('Servers test (with setup and tear-down) ', () => {
-//   beforeEach(()=>{
-//     serverName.value = 'Alice';
-//   })
-//   it(`should add serverName in allServerName if serverName doesn't exist `), ()=>{
-//     expect(allServerName.length).toEqual(1);
-//   }
-//   it(`should not add serverName in allServerName if serverName already exists regardless of case sensitive and space before and after serverNa  `), ()=>{
-//     allServerName = ['Alice'];
-//     serverName.value = ' alice'
-//     expect(allServerName.length).toEqual(1);
-//   }
-//   it(`should not take empty string`), ()=>{
-//     serverName.value = " ";
-//     expect(allServerName.length).toEqual(0);
-//   }
-//   afterEach(()=>{
-//     allServerName = [];
-//     serverName.value = "";
-//   })
+describe('Servers test (with setup and tear-down) ', () => {
+  beforeEach(()=>{
+    serverName.value = 'Alice';
+  })
+  it(`should add serverName in allServerName if serverName doesn't exist `), ()=>{
+    expect(allServerName.length).toEqual(1);
+  }
+  it(`should not add serverName in allServerName if serverName already exists regardless of case sensitive and space before and after serverNa  `), ()=>{
+    allServerName = ['Alice'];
+    serverName.value = ' alice'
+    expect(allServerName.length).toEqual(1);
+  }
+  it(`should not take empty string`), ()=>{
+    serverName.value = " ";
+    expect(allServerName.length).toEqual(0);
+  }
+  afterEach(()=>{
+    allServerName = [];
+    serverName.value = "";
+  })
 
-// });
+});
